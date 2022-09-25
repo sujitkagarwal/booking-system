a44b35aa7080cdb3e117d17840102992487e16e4
sujitagarwal <sujit21agarwal@gmail.com>
2022-09-25 12:23:45 +0530

    inital commit 3

--- theater/src/main/java/com/assessment/theater/service/cinema/impl/CinemaServiceImpl.java
+++ theater/src/main/java/com/assessment/theater/service/cinema/impl/CinemaServiceImpl.java
@@ -3,6 +3,7 @@ package com.assessment.theater.service.cinema.impl;
 import com.assessment.theater.entity.city.City;
 import com.assessment.theater.model.city.CityRequest;
 import com.assessment.theater.repository.CinemaRepository;
+import com.assessment.theater.repository.CityRepository;
 import com.assessment.theater.service.cinema.CinemaService;
 import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
@@ -13,10 +14,10 @@ import org.springframework.stereotype.Service;
 public class CinemaServiceImpl implements CinemaService {
 
     private final Converter<CityRequest, City> cityConverter;
-    private final CinemaRepository cinemaRepository;
+    private final CityRepository cityRepository;
 
     @Override
     public void onboardCinema(CityRequest cityRequest) {
-        cinemaRepository.save(cityConverter.convert(cityRequest));
+        cityRepository.save(cityConverter.convert(cityRequest));
     }
 }
